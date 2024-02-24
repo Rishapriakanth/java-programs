3cfa9a237c8b05e51f464686e7f9f26376eec56c
Rishapriakanth <108320843+Rishapriakanth@users.noreply.github.com>
2024-02-24 17:13:43 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+//object and class :main outside the class
+class Student{  
+ int id;  
+ String name;  
+}  
+class Main{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+ }  
+}  
