3c26620387eae71f358eab06fccfb61b88595004
Rishapriakanth <108320843+Rishapriakanth@users.noreply.github.com>
2024-03-07 22:47:08 +0530

    Create reverse each word of string.java

---
+++ reverse each word of string.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String reversedStr = reverseEachWord(str);
+        System.out.println(reversedStr);
+    }
+
+    public static String reverseEachWord(String str) {
+        String[] words = str.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i > 0) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+}
