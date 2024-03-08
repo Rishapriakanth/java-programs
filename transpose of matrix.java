6591c7c48f5c7e8bb8eeb2668f691b5b64bf8eef
Rishapriakanth <108320843+Rishapriakanth@users.noreply.github.com>
2024-03-08 21:07:35 +0530

    Create transpose of matrix.java

---
+++ transpose of matrix.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        printMatrix(matrix);
+        int[][] transpose = calculateTranspose(matrix);
+        System.out.println("Transpose matrix is:");
+        printMatrix(transpose);
+    }
+
+    public static int[][] calculateTranspose(int[][] matrix) {
+        int n = matrix.length;
+        int[][] transpose = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
